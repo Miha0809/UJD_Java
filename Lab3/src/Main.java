613c191bec259e50672a36800ce81808f5e01c08
Miha0809 <miha080104@gmail.com>
2024-04-15 11:20:15 +0200

    Create navigation

--- Lab3/src/Main.java
+++ Lab3/src/Main.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        var readDirectories = new ReadDirectories();
-        readDirectories.Read("ajp-2024-w03");
+        Scanner scanner = new Scanner(System.in);
+        int mode = -1;
+
+        while (mode != 0)
+        {
+            System.out.println("NAVIGATION");
+            System.out.println("Enter 1 - task1");
+            System.out.println("Enter 2 - task2");
+            System.out.println("Enter 2 - task2");
+            mode = scanner.nextInt();
+
+            switch (mode)
+            {
+                case 1: {
+                    var readDirectories = new ReadDirectories();
+                    readDirectories.Read("ajp-2024-w03");
+                    break;
+                }
+                case 2: {
+                    var readFiles = new ReadFiles();
+                    readFiles.Read("ajp-2024-w03");
+                    break;
+                }
+            }
+        }
     }
 }
