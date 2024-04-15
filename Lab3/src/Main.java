12174c31177e322465baa994ed4920e2aea5997e
Miha0809 <miha080104@gmail.com>
2024-04-15 10:36:57 +0200

    Task1

--- Lab3/src/Main.java
+++ Lab3/src/Main.java
@@ -1,15 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Read("../ajp-2024-w03");
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    private static void Read(String path)
+    {
+        File file = new File(path);
+
+        for (File f : file.listFiles())
+        {
+            if (f.isDirectory())
+            {
+                System.out.println(f.getPath());
+                Read(f.getPath());
+            }
         }
     }
 }
