412e3a80b66e4774a993591227f8d05ce5652d55
Miha0809 <miha080104@gmail.com>
2024-05-04 20:02:55 +0200

    Task 1 and navigation

--- Lab4/src/Main.java
+++ Lab4/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 class RandomSequence implements IntSequence {
     private static Random generator = new Random();
@@ -23,16 +24,50 @@ class RandomSequence implements IntSequence {
 
 public class Main {
 
-    public static IntSequence randomInts(int low, int high) {
+    private static Runnable foo(Runnable... runnables) {
+        return () -> {
+            for (Runnable runnable : runnables) {
+                runnable.run();
+            }
+        };
+    }
+
+    private static IntSequence randomInts(int low, int high) {
         var numbers = new RandomSequence(low, high);
         return numbers;
     }
 
     public static void main(String[] args) {
-        var numbers = randomInts(5, 10);
+        Scanner scanner = new Scanner(System.in);
+        int mode = -1;
 
-        while (numbers.hasNext()) {
-            System.out.println(numbers.next());
+        while (mode != 0) {
+            System.out.println("\tNAVIGATION");
+            System.out.println("Task 1 - enter 1");
+            System.out.println("Task 2 - enter 2");
+            System.out.println("Task 3 - enter 3");
+            System.out.println("Exit - other");
+            mode = scanner.nextInt();
+
+            switch (mode) {
+                case 1: {
+                    Runnable runnable1 = () -> System.out.println("runnable1");
+                    Runnable runnable2 = () -> System.out.println("runnable2");
+                    Runnable runnable3 = () -> System.out.println("runnable3");
+                    Runnable runnable = foo(runnable1, runnable2, runnable3);
+                    runnable.run();
+                    break;
+                }
+                case 3: {
+                    var numbers = randomInts(5, 10);
+
+                    while (numbers.hasNext()) {
+                        System.out.println(numbers.next());
+                    }
+                }
+                default:
+                    break;
+            }
         }
     }
 }
