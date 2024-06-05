c81a0053d2d2436077a8145fbe387548bf19d204
Miha0809 <miha080104@gmail.com>
2024-06-05 12:25:34 +0200

    Task2

--- Lab7/src/Main.java
+++ Lab7/src/Main.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 import java.util.Optional;
 import java.util.Map;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -59,8 +61,30 @@ public class Main {
         System.out.println(emailsByAge);
     }
 
+    private static <T> boolean isFinite(Stream<T> stream) {
+        try {
+            Iterator<T> iterator = stream.iterator();
+            long count = 0;
+            while (iterator.hasNext()) {
+                iterator.next();
+                count++;
+                if (count > Long.MAX_VALUE - 1) {
+                    return false;
+                }
+            }
+            return true;
+        } catch (OutOfMemoryError | StackOverflowError e) {
+            return false;
+        } catch (Exception e) {
+            return false;
+        }
+    }
     private static void Task2() {
+        Stream<Integer> finiteStream = Stream.of(1, 2, 3, 4, 5);
+        Stream<Integer> infiniteStream = Stream.iterate(0, n -> n + 1);
 
+        System.out.println(isFinite(finiteStream));
+        System.out.println(isFinite(infiniteStream));
     }
 
     private static void Task3() {
